b05a7f2cb6d7984645f3abbf554f9ac9cbcc67ea
David-Kolar <79227617+David-Kolar@users.noreply.github.com>
2021-02-24 17:18:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-class Cislo():
+class Number():
     def __init__(self, value):
         self.value = value
     def __add__(self, other):
-        return Cislo(self.value + other.value)
+        return Number(self.value + other.value)
     def __sub__(self, other):
-        return Cislo(self.value - other.value)
+        return Number(self.value - other.value)
     def __mul__(self, other):
-        return Cislo(self.value * other.value)
+        return Number(self.value * other.value)
     def __truediv__(self, other):
-        return Cislo(self.value / other.value)
+        return Number(self.value / other.value)
     def __str__(self):
         return str(self.value)
-vysledek = eval("Cislo(5)*Cislo(-2) + Cislo(-7) - (Cislo(3) + Cislo(4)/Cislo(2))")
-print(vysledek)
-a = 4+3j
-a *= 2
-print(a)
+solution = eval("Number(5)*Number(-2)")
+print(solution)
